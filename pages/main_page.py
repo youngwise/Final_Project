82493e127ccee54166185046beb31c8e46f6ef21
youngwise <sergeylazarev2004@gmail.com>
2022-01-20 23:15:04 +0300

    Added processing alert; import optimization

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,4 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 
 
@@ -7,6 +6,8 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link isn\'t presented'
