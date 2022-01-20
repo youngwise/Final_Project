fa5d838e55b906cb66b179f89b741775764e40bc
Sergey <sergeylazarev2004@gmail.com>
2022-01-20 15:53:32 +0300

    Implementation of LoginPage: checks the correctness of the link, checks for the presence of a registration and login form

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.ID, 'login_link')
+
+
+class LoginPageLocators:
+    REGISTER_FORM = (By.ID, 'register_form')
+    LOGIN_FORM = (By.ID, 'login_form')
