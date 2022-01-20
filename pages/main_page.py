4d8efe19a28b97864ff091a664fb8dfdd0c54301
Sergey <sergeylazarev2004@gmail.com>
2022-01-20 12:22:49 +0300

    Implementation of storing element selectors in separate classes inside locators.py, replacing locators in tests

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,12 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.ID, 'login_link')
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.ID, 'login_link_invalid'), 'Login link isn\'t presented'
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link isn\'t presented'
