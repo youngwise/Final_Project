a1c88ae9b5486613e5765b52e650bf2281efdf91
Sergey <sergeylazarev2004@gmail.com>
2022-01-20 10:27:22 +0300

    Creating a file that stores methods for working from the main page

---
+++ pages/main_page.py
@@ -0,0 +1,8 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.ID, 'login_link')
+        login_link.click()
