4d92259beb7b97c502aaea73467b7752be00e226
Sergey <sergeylazarev2004@gmail.com>
2022-01-20 11:45:59 +0300

    Added helper method is_element_present to find an element on the base page

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,3 +6,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.ID, 'login_link')
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.ID, 'login_link_invalid'), 'Login link isn\'t presented'
