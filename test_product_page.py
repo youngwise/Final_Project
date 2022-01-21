80cfaf4e5f290c3fe4fdd53fc6f0216bc5f4a1ad
Sergey <sergeylazarev2004@gmail.com>
2022-01-21 15:41:12 +0300

    Added tests to check that there is no success message

--- test_product_page.py
+++ test_product_page.py
@@ -18,3 +18,28 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.open()
     page.add_to_basket()
     page.should_be_product_add_to_basket()
+
+
+link_test = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link_test)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link_test)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link_test)
+    page.open()
+    page.add_to_basket()
+    page.should_be_message_disappeared()
